038fb9a6bd5b24e2e4f8afc591184b1b77e589de
Xuan Tan <xt307@nyu.edu>
2018-02-23 00:58:44 -0800

    add more

--- rcnn/tools/train_rpn.py
+++ rcnn/tools/train_rpn.py
@@ -17,31 +17,27 @@
 
 import argparse
 import pprint
-import mxnet as mx
 
-from ..logger import logger
-from ..config import config, default, generate_config
-from ..symbol import *
-from ..core import callback, metric
-from ..core.loader import ROIIter
+from ..config import default, generate_config
+from ..core import metric
+from ..core.loader import AnchorLoader
 from ..core.module import MutableModule
-from ..processing.bbox_regression import add_bbox_regression_targets
-from ..utils.load_data import load_proposal_roidb, merge_roidb, filter_roidb
+from ..logger import logger
+from ..symbol import *
+from ..utils.load_data import load_gt_roidb, merge_roidb, filter_roidb
 from ..utils.load_model import load_param
 
 
-def train_rcnn(network, dataset, image_set, root_path, dataset_path,
-               frequent, kvstore, work_load_list, no_flip, no_shuffle, resume,
-               ctx, pretrained, epoch, prefix, begin_epoch, end_epoch,
-               train_shared, lr, lr_step, proposal):
-    # set up config
-    config.TRAIN.BATCH_IMAGES = 2
-    config.TRAIN.BATCH_ROIS = 128
-    if proposal == 'ss':
-        config.TRAIN.BG_THRESH_LO = 0.1  # reproduce Fast R-CNN
+def train_rpn(network, dataset, image_set, root_path, dataset_path,
+              frequent, kvstore, work_load_list, no_flip, no_shuffle, resume,
+              ctx, pretrained, epoch, prefix, begin_epoch, end_epoch,
+              train_shared, lr, lr_step):
+    # setup config
+    config.TRAIN.BATCH_IMAGES = 1
 
     # load symbol
-    sym = eval('get_' + network + '_rcnn')(num_classes=config.NUM_CLASSES)
+    sym = eval('get_' + network + '_rpn')(num_anchors=config.NUM_ANCHORS)
+    feat_sym = sym.get_internals()['rpn_cls_score_output']
 
     # setup multi-gpu
     batch_size = len(ctx)
@@ -52,20 +48,22 @@ def train_rcnn(network, dataset, image_set, root_path, dataset_path,
 
     # load dataset and prepare imdb for training
     image_sets = [iset for iset in image_set.split('+')]
-    roidbs = [load_proposal_roidb(dataset, image_set, root_path, dataset_path,
-                                  proposal=proposal, append_gt=True, flip=not no_flip)
+    roidbs = [load_gt_roidb(dataset, image_set, root_path, dataset_path,
+                            flip=not no_flip)
               for image_set in image_sets]
     roidb = merge_roidb(roidbs)
     roidb = filter_roidb(roidb)
-    means, stds = add_bbox_regression_targets(roidb)
 
     # load training data
-    train_data = ROIIter(roidb, batch_size=input_batch_size, shuffle=not no_shuffle,
-                         ctx=ctx, work_load_list=work_load_list, aspect_grouping=config.TRAIN.ASPECT_GROUPING)
+    train_data = AnchorLoader(feat_sym, roidb, batch_size=input_batch_size, shuffle=not no_shuffle,
+                              ctx=ctx, work_load_list=work_load_list,
+                              feat_stride=config.RPN_FEAT_STRIDE, anchor_scales=config.ANCHOR_SCALES,
+                              anchor_ratios=config.ANCHOR_RATIOS, aspect_grouping=config.TRAIN.ASPECT_GROUPING)
 
     # infer max shape
     max_data_shape = [('data', (input_batch_size, 3, max([v[0] for v in config.SCALES]), max([v[1] for v in config.SCALES])))]
-    logger.info('providing maximum shape %s' % max_data_shape)
+    max_data_shape, max_label_shape = train_data.infer_shape(max_data_shape)
+    logger.info('providing maximum shape %s %s' % (max_data_shape, max_label_shape))
 
     # infer shape
     data_shape_dict = dict(train_data.provide_data + train_data.provide_label)
@@ -80,10 +78,12 @@ def train_rcnn(network, dataset, image_set, root_path, dataset_path,
         arg_params, aux_params = load_param(prefix, begin_epoch, convert=True)
     else:
         arg_params, aux_params = load_param(pretrained, epoch, convert=True)
-        arg_params['cls_score_weight'] = mx.random.normal(0, 0.01, shape=arg_shape_dict['cls_score_weight'])
-        arg_params['cls_score_bias'] = mx.nd.zeros(shape=arg_shape_dict['cls_score_bias'])
-        arg_params['bbox_pred_weight'] = mx.random.normal(0, 0.001, shape=arg_shape_dict['bbox_pred_weight'])
-        arg_params['bbox_pred_bias'] = mx.nd.zeros(shape=arg_shape_dict['bbox_pred_bias'])
+        arg_params['rpn_conv_3x3_weight'] = mx.random.normal(0, 0.01, shape=arg_shape_dict['rpn_conv_3x3_weight'])
+        arg_params['rpn_conv_3x3_bias'] = mx.nd.zeros(shape=arg_shape_dict['rpn_conv_3x3_bias'])
+        arg_params['rpn_cls_score_weight'] = mx.random.normal(0, 0.01, shape=arg_shape_dict['rpn_cls_score_weight'])
+        arg_params['rpn_cls_score_bias'] = mx.nd.zeros(shape=arg_shape_dict['rpn_cls_score_bias'])
+        arg_params['rpn_bbox_pred_weight'] = mx.random.normal(0, 0.01, shape=arg_shape_dict['rpn_bbox_pred_weight'])
+        arg_params['rpn_bbox_pred_bias'] = mx.nd.zeros(shape=arg_shape_dict['rpn_bbox_pred_bias'])
 
     # check parameter shapes
     for k in sym.list_arguments():
@@ -97,7 +97,6 @@ def train_rcnn(network, dataset, image_set, root_path, dataset_path,
         assert aux_params[k].shape == aux_shape_dict[k], \
             'shape inconsistent for ' + k + ' inferred ' + str(aux_shape_dict[k]) + ' provided ' + str(aux_params[k].shape)
 
-    # prepare training
     # create solver
     data_names = [k[0] for k in train_data.provide_data]
     label_names = [k[0] for k in train_data.provide_label]
@@ -107,19 +106,20 @@ def train_rcnn(network, dataset, image_set, root_path, dataset_path,
         fixed_param_prefix = config.FIXED_PARAMS
     mod = MutableModule(sym, data_names=data_names, label_names=label_names,
                         logger=logger, context=ctx, work_load_list=work_load_list,
-                        max_data_shapes=max_data_shape, fixed_param_prefix=fixed_param_prefix)
+                        max_data_shapes=max_data_shape, max_label_shapes=max_label_shape,
+                        fixed_param_prefix=fixed_param_prefix)
 
     # decide training params
     # metric
-    eval_metric = metric.RCNNAccMetric()
-    cls_metric = metric.RCNNLogLossMetric()
-    bbox_metric = metric.RCNNL1LossMetric()
+    eval_metric = metric.RPNAccMetric()
+    cls_metric = metric.RPNLogLossMetric()
+    bbox_metric = metric.RPNL1LossMetric()
     eval_metrics = mx.metric.CompositeEvalMetric()
     for child_metric in [eval_metric, cls_metric, bbox_metric]:
         eval_metrics.add(child_metric)
     # callback
     batch_end_callback = mx.callback.Speedometer(train_data.batch_size, frequent=frequent, auto_reset=False)
-    epoch_end_callback = callback.do_checkpoint(prefix, means, stds)
+    epoch_end_callback = mx.callback.do_checkpoint(prefix)
     # decide learning rate
     base_lr = lr
     lr_factor = 0.1
@@ -145,7 +145,7 @@ def train_rcnn(network, dataset, image_set, root_path, dataset_path,
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train a Fast R-CNN Network')
+    parser = argparse.ArgumentParser(description='Train a Region Proposal Network')
     # general
     parser.add_argument('--network', help='network name', default=default.network, type=str)
     parser.add_argument('--dataset', help='dataset name', default=default.dataset, type=str)
@@ -161,17 +161,16 @@ def parse_args():
     parser.add_argument('--no_flip', help='disable flip images', action='store_true')
     parser.add_argument('--no_shuffle', help='disable random shuffle', action='store_true')
     parser.add_argument('--resume', help='continue training', action='store_true')
-    # rcnn
+    # rpn
     parser.add_argument('--gpus', help='GPU device to train with', default='0', type=str)
     parser.add_argument('--pretrained', help='pretrained model prefix', default=default.pretrained, type=str)
     parser.add_argument('--pretrained_epoch', help='pretrained model epoch', default=default.pretrained_epoch, type=int)
-    parser.add_argument('--prefix', help='new model prefix', default=default.rcnn_prefix, type=str)
+    parser.add_argument('--prefix', help='new model prefix', default=default.rpn_prefix, type=str)
     parser.add_argument('--begin_epoch', help='begin epoch of training', default=0, type=int)
-    parser.add_argument('--end_epoch', help='end epoch of training', default=default.rcnn_epoch, type=int)
-    parser.add_argument('--lr', help='base learning rate', default=default.rcnn_lr, type=float)
-    parser.add_argument('--lr_step', help='learning rate steps (in epoch)', default=default.rcnn_lr_step, type=str)
+    parser.add_argument('--end_epoch', help='end epoch of training', default=default.rpn_epoch, type=int)
+    parser.add_argument('--lr', help='base learning rate', default=default.rpn_lr, type=float)
+    parser.add_argument('--lr_step', help='learning rate steps (in epoch)', default=default.rpn_lr_step, type=str)
     parser.add_argument('--train_shared', help='second round train shared params', action='store_true')
-    parser.add_argument('--proposal', help='can be ss for selective search or rpn', default='rpn', type=str)
     args = parser.parse_args()
     return args
 
@@ -180,10 +179,10 @@ def main():
     args = parse_args()
     logger.info('Called with argument: %s' % args)
     ctx = [mx.gpu(int(i)) for i in args.gpus.split(',')]
-    train_rcnn(args.network, args.dataset, args.image_set, args.root_path, args.dataset_path,
-               args.frequent, args.kvstore, args.work_load_list, args.no_flip, args.no_shuffle, args.resume,
-               ctx, args.pretrained, args.pretrained_epoch, args.prefix, args.begin_epoch, args.end_epoch,
-               train_shared=args.train_shared, lr=args.lr, lr_step=args.lr_step, proposal=args.proposal)
+    train_rpn(args.network, args.dataset, args.image_set, args.root_path, args.dataset_path,
+              args.frequent, args.kvstore, args.work_load_list, args.no_flip, args.no_shuffle, args.resume,
+              ctx, args.pretrained, args.pretrained_epoch, args.prefix, args.begin_epoch, args.end_epoch,
+              train_shared=args.train_shared, lr=args.lr, lr_step=args.lr_step)
 
 if __name__ == '__main__':
     main()
